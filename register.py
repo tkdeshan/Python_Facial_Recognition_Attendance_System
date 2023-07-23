436f3d37a868e5c249770fecb37f64fb311f2bd6
tkdeshan <tkdeshan1103@gmail.com>
2023-07-23 17:22:08 +0530

    create insert query

--- register.py
+++ register.py
@@ -8,6 +8,12 @@ from PIL import Image, ImageTk
 
 def create_registration_window():
 
+    def exit_application():
+        root.destroy()
+
+    def back_to_main():
+        pass
+
     def GetValue(event):
         e1.delete(0, END)
         e2.delete(0, END)
@@ -32,6 +38,37 @@ def create_registration_window():
             listBox.insert("", "end", values=(emplyid, emplyname, mobile, email))
 
         mysqldb.close()
+
+
+    def Add():
+        emplyid = e1.get()
+        emplyname = e2.get()
+        mobile = e3.get()
+        email = e4.get()
+        img = img_data
+
+        mysqldb = pymysql.connect(host="localhost", user="root", password="", database="pfrsdb")
+        mycursor = mysqldb.cursor()
+
+        try:
+            sql = "INSERT INTO  registration (Id,Name,Mobile,Email,Image) VALUES (%s, %s, %s, %s, %s)"
+            val = (emplyid, emplyname, mobile, email, img)
+            mycursor.execute(sql, val)
+            mysqldb.commit()
+            lastid = mycursor.lastrowid
+            messagebox.showinfo("information", "Employee inserted successfully...")
+            e1.delete(0, END)
+            e2.delete(0, END)
+            e3.delete(0, END)
+            e4.delete(0, END)
+            image_label.config(image=None)
+            image_label.image = None
+            e1.focus_set()
+        except Exception as e:
+            print(e)
+            mysqldb.rollback()
+            mysqldb.close()
+
 
     def convert_image_to_binary_data(image_path):
         with open(image_path, 'rb') as file:
@@ -91,11 +128,11 @@ def create_registration_window():
     e4 = Entry(root, font=(None, 11))
     e4.place(x=140, y=130)
 
-    Button(root, text="Add",  height=2, width=10, font=(None, 11)).place(x=10, y=180)
+    Button(root, text="Add", command=Add, height=2, width=10, font=(None, 11)).place(x=10, y=180)
     Button(root, text="update", height=2, width=10, font=(None, 11)).place(x=130, y=180)
     Button(root, text="Delete",  height=2, width=10, font=(None, 11)).place(x=250, y=180)
-    Button(root, text="Back",  height=2, width=10, font=(None, 11)).place(x=370, y=180)
-    Button(root, text="Exit",  height=2, width=10, font=(None, 11)).place(x=490, y=180)
+    Button(root, text="Back", command=back_to_main, height=2, width=10, font=(None, 11)).place(x=370, y=180)
+    Button(root, text="Exit",  command=exit_application, height=2, width=10, font=(None, 11)).place(x=490, y=180)
 
     cols = ('Id', 'Name', 'Mobile', 'Email')
     listBox = ttk.Treeview(root, columns=cols, show='headings')
