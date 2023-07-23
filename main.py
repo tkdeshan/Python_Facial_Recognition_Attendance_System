c82b8ef8c90ca65440778f4361a852cbaf3c0799
tkdeshan <tkdeshan1103@gmail.com>
2023-07-23 16:12:35 +0530

    create main screen

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from tkinter import *
 
+root = Tk()
+root.title("Face Recognition System")
+root.geometry("800x485+300+200")
+root.resizable(False, False)
+
+frame_label = Label(root)
+frame_label.place(x=2, y=2)
+
+
+Button(root, text="Present", height=2, width=10, font=(None, 11)).place(x=670, y=100)
+Button(root, text="Add new", height=2, width=10, font=(None, 11)).place(x=670, y=200)
+Button(root, text="Exit", height=2, width=10, font=(None, 11)).place(x=670, y=300)
+
+root.mainloop()
