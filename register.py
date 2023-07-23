73742fd5b2a66d9b6addbd2eb689dd088cc92d49
tkdeshan <tkdeshan1103@gmail.com>
2023-07-23 17:11:49 +0530

    retrive data from database

--- register.py
+++ register.py
@@ -2,10 +2,61 @@ import os
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
+
+import pymysql
 from PIL import Image, ImageTk
 
 def create_registration_window():
 
+    def GetValue(event):
+        e1.delete(0, END)
+        e2.delete(0, END)
+        e3.delete(0, END)
+        e4.delete(0, END)
+
+        row_id = listBox.selection()[0]
+        select = listBox.set(row_id)
+        e1.insert(0, select['id'])
+        e2.insert(0, select['empname'])
+        e3.insert(0, select['mobile'])
+        e4.insert(0, select['salary'])
+
+    def show():
+        mysqldb = pymysql.connect(host="localhost", user="root", password="", database="pfrsdb")
+        mycursor = mysqldb.cursor()
+        mycursor.execute("SELECT Id,Name,Mobile,Email FROM registration")
+        records = mycursor.fetchall()
+        print(records)
+
+        for i, (emplyid, emplyname, mobile, email) in enumerate(records, start=1):
+            listBox.insert("", "end", values=(emplyid, emplyname, mobile, email))
+
+        mysqldb.close()
+
+    def convert_image_to_binary_data(image_path):
+        with open(image_path, 'rb') as file:
+            binary_data = file.read()
+        return binary_data
+
+    def upload_image():
+        global img_data
+        global image
+
+        file_path = filedialog.askopenfilename()
+        img_data = convert_image_to_binary_data(file_path)
+
+        temp_file_path = "temp_image.jpg"
+        with open(temp_file_path, 'wb') as file:
+            file.write(img_data)
+
+        image = Image.open(temp_file_path)
+        image = image.resize((200, 200))
+        photo = ImageTk.PhotoImage(image)
+
+        image_label.config(image=photo)
+        image_label.image = photo
+
+        os.remove(temp_file_path)
 
 
     root = Tk()
@@ -19,7 +70,7 @@ def create_registration_window():
     Label(root, text="Mobile", font=(None, 11)).place(x=10, y=100)
     Label(root, text="Email", font=(None, 11)).place(x=10, y=130)
 
-    Button(root, text="Upload Image", font=(None, 11)).place(x=400, y=35)
+    Button(root, text="Upload Image", command=upload_image, font=(None, 11)).place(x=400, y=35)
     image_label = Label(root, text="Image", font=(None, 11))
     image_label.place(x=550, y=35)
 
@@ -55,8 +106,9 @@ def create_registration_window():
         listBox.place(x=10, y=250)
 
 
-
-
+    listBox.bind('<Double-Button-1>', GetValue)
+    show()
     root.mainloop()
+
 
 create_registration_window()
