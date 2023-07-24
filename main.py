a5195ecd628c6e4aac41beaf1fbcf87867b1fa41
tkdeshan <tkdeshan1103@gmail.com>
2023-07-24 21:01:19 +0530

    compress code to one page

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
+import tkinter as tk
 from tkinter import *
+from tkinter import ttk, messagebox, filedialog
+
 import cv2
 import face_recognition as fr
 from PIL import Image, ImageTk
@@ -6,12 +10,15 @@ import numpy as np
 import pyttsx3
 import pymysql
 
-from register import create_registration_window
+
+def show_register_window():
+    root.withdraw()
+    register_window.deiconify()
 
 
-def register():
-    root.destroy()  # Close the main window
-    create_registration_window()
+def show_main_window():
+    register_window.withdraw()
+    root.deiconify()
 
 
 def exit_application():
@@ -51,6 +58,7 @@ def retrieve_images_and_names_from_database():
         print("Error retrieving images and names:", e)
         return [], []
 
+
 known_face_encodings = []
 known_face_names = []
 
@@ -66,9 +74,9 @@ for image_data, name in zip(images_from_database, names_from_database):
         known_face_names.append(name)
 
 
-root = Tk()
-root.title("Face Recognition System")
 
+root = tk.Tk()
+root.title("Main Window")
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 x = (screen_width - 800) // 2
@@ -82,6 +90,7 @@ frame_label.place(x=2, y=2)
 video_capture = cv2.VideoCapture(0)
 
 text = "Please try again."
+
 
 def update_frame():
     global text
@@ -118,8 +127,208 @@ def update_frame():
 update_frame()
 
 Button(root, text="Present", command=lambda: speaknow(text), height=2, width=10, font=(None, 11)).place(x=670, y=100)
-Button(root, text="Add new", command=register, height=2, width=10, font=(None, 11)).place(x=670, y=200)
+Button(root, text="Add new", command=show_register_window, height=2, width=10, font=(None, 11)).place(x=670, y=200)
 Button(root, text="Exit", command=exit_application, height=2, width=10, font=(None, 11)).place(x=670, y=300)
+
+
+
+# register window
+
+def GetValue(event):
+    e1.delete(0, END)
+    e2.delete(0, END)
+    e3.delete(0, END)
+    e4.delete(0, END)
+
+    row_id = listBox.selection()[0]
+    select = listBox.set(row_id)
+    e1.insert(0, select['id'])
+    e2.insert(0, select['empname'])
+    e3.insert(0, select['mobile'])
+    e4.insert(0, select['salary'])
+
+
+def convert_image_to_binary_data(image_path):
+    with open(image_path, 'rb') as file:
+        binary_data = file.read()
+    return binary_data
+
+
+def upload_image():
+    global img_data
+    global image
+
+    file_path = filedialog.askopenfilename()
+    img_data = convert_image_to_binary_data(file_path)
+
+    temp_file_path = "temp_image.jpg"
+    with open(temp_file_path, 'wb') as file:
+        file.write(img_data)
+
+    image = Image.open(temp_file_path)
+    image = image.resize((200, 200))
+    photo = ImageTk.PhotoImage(image)
+
+    image_label.config(image=photo)
+    image_label.image = photo
+
+    os.remove(temp_file_path)
+
+
+def Add():
+    emplyid = e1.get()
+    emplyname = e2.get()
+    mobile = e3.get()
+    email = e4.get()
+    img = img_data
+
+    mysqldb = pymysql.connect(host="localhost", user="root", password="", database="pfrsdb")
+    mycursor = mysqldb.cursor()
+
+    try:
+        sql = "INSERT INTO  registration (Id,Name,Mobile,Email,Image) VALUES (%s, %s, %s, %s, %s)"
+        val = (emplyid, emplyname, mobile, email, img)
+        mycursor.execute(sql, val)
+        mysqldb.commit()
+        lastid = mycursor.lastrowid
+        messagebox.showinfo("information", "Employee inserted successfully...")
+        e1.delete(0, END)
+        e2.delete(0, END)
+        e3.delete(0, END)
+        e4.delete(0, END)
+        image_label.config(image=None)
+        image_label.image = None
+        e1.focus_set()
+    except Exception as e:
+        print(e)
+        mysqldb.rollback()
+        mysqldb.close()
+
+
+def update():
+    emplyid = e1.get()
+    emplyname = e2.get()
+    mobile = e3.get()
+    email = e4.get()
+    img = img_data
+
+    mysqldb = pymysql.connect(host="localhost", user="root", password="", database="pfrsdb")
+    mycursor = mysqldb.cursor()
+
+    try:
+        sql = "Update registration set Name= %s,Mobile= %s,Email= %s,Image=%s where id= %s"
+        val = (emplyname, mobile, email, img, emplyid)
+        mycursor.execute(sql, val)
+        mysqldb.commit()
+        lastid = mycursor.lastrowid
+        messagebox.showinfo("information", "Record Updateddddd successfully...")
+
+        e1.delete(0, END)
+        e2.delete(0, END)
+        e3.delete(0, END)
+        e4.delete(0, END)
+        image_label.config(image=None)
+        image_label.image = None
+        e1.focus_set()
+
+    except Exception as e:
+
+        print(e)
+        mysqldb.rollback()
+        mysqldb.close()
+
+
+def delete():
+    emplyid = e1.get()
+
+    mysqldb = pymysql.connect(host="localhost", user="root", password="", database="pfrsdb")
+    mycursor = mysqldb.cursor()
+
+    try:
+        sql = "delete from registration where id = %s"
+        val = (emplyid)
+        mycursor.execute(sql, val)
+        mysqldb.commit()
+        lastid = mycursor.lastrowid
+        messagebox.showinfo("information", "Record Deleteeeee successfully...")
+
+        e1.delete(0, END)
+        e2.delete(0, END)
+        e3.delete(0, END)
+        e4.delete(0, END)
+        e1.focus_set()
+
+    except Exception as e:
+
+        print(e)
+        mysqldb.rollback()
+        mysqldb.close()
+
+
+def show():
+    mysqldb = pymysql.connect(host="localhost", user="root", password="", database="pfrsdb")
+    mycursor = mysqldb.cursor()
+    mycursor.execute("SELECT Id,Name,Mobile,Email FROM registration")
+    records = mycursor.fetchall()
+    print(records)
+
+    for i, (emplyid, emplyname, mobile, email) in enumerate(records, start=1):
+        listBox.insert("", "end", values=(emplyid, emplyname, mobile, email))
+
+    mysqldb.close()
+
+
+register_window = tk.Toplevel(root)
+register_window.title("Register Window")
+screen_width1 = register_window.winfo_screenwidth()
+screen_height1 = register_window.winfo_screenheight()
+a = (screen_width1 - 800) // 2
+b = (screen_height1 - 485) // 2
+register_window.geometry(f"800x485+{a}+{b}")
+register_window.resizable(False, False)
+register_window.withdraw()  # Hide the second window initially
+
+tk.Label(register_window, text="Employee Registration", fg="red", font=(None, 15)).place(x=290, y=5)
+
+tk.Label(register_window, text="Employee ID", font=(None, 11)).place(x=10, y=40)
+Label(register_window, text="Employee Name", font=(None, 11)).place(x=10, y=70)
+Label(register_window, text="Mobile", font=(None, 11)).place(x=10, y=100)
+Label(register_window, text="Email", font=(None, 11)).place(x=10, y=130)
+
+Button(register_window, text="Upload Image", command=upload_image, font=(None, 11)).place(x=400, y=35)
+image_label = Label(register_window, text="Image", font=(None, 11))
+image_label.place(x=550, y=35)
+
+e1 = Entry(register_window, font=(None, 11))
+e1.place(x=140, y=40)
+
+e2 = Entry(register_window, font=(None, 11))
+e2.place(x=140, y=70)
+
+e3 = Entry(register_window, font=(None, 11))
+e3.place(x=140, y=100)
+
+e4 = Entry(register_window, font=(None, 11))
+e4.place(x=140, y=130)
+
+Button(register_window, text="Add", command=Add, height=2, width=10, font=(None, 11)).place(x=10, y=180)
+Button(register_window, text="update", command=update, height=2, width=10, font=(None, 11)).place(x=130, y=180)
+Button(register_window, text="Delete", command=delete, height=2, width=10, font=(None, 11)).place(x=250, y=180)
+Button(register_window, text="Back", command=show_main_window, height=2, width=10, font=(None, 11)).place(x=370,
+                                                                                                          y=180)
+Button(register_window, text="Exit", command=exit_application, height=2, width=10, font=(None, 11)).place(x=490,
+                                                                                                          y=180)
+
+cols = ('Id', 'Name', 'Mobile', 'Email')
+listBox = ttk.Treeview(register_window, columns=cols, show='headings')
+
+for col in cols:
+    listBox.heading(col, text=col, )
+    listBox.grid(row=1, column=0, columnspan=2)
+    listBox.place(x=10, y=250)
+
+show()
+listBox.bind('<Double-Button-1>', GetValue)
 
 root.mainloop()
 
